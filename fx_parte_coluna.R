7cee3a505808802e586f46b91040312d516893e6
gusmmm <jgustavomartins@gmail.com>
2016-10-05 14:17:59 +0100

    primeira funçao adicionada, parte a coluna complexa e devolve uma tabela com 2 colunas, com o identificador e com a informaçao separada da coluna complexa

---
+++ fx_parte_coluna.R
@@ -0,0 +1,57 @@
+# primeira função#
+# para usar em colunas com mais do que uma informação por célula
+# separadas com simbolo pre-definido: ex: - , +
+# devolve uma tabela com 2 colunas: identificador info
+
+### packages necessárias ###
+# library(stringr)
+# library(dplyr)
+
+### pre-requisitos ###
+
+#1 criar um objecto com os separadores
+# ex com 2 separadores, o + e a ,:
+# > separadores <- c("+",",")
+# se não for criado, a função usa o + como separador padrão
+
+fx_parte_coluna <- function(coluna_id, coluna_complexa,separadores = "+"){
+  #print(coluna_id)
+  #print(coluna_complexa)
+  
+  # transforma separadores em regex
+  separadores <- paste(separadores,sep = "",collapse = "|")
+  separadores <- str_replace(string = separadores,pattern = "\\+","\\\\+")
+  
+  # cria a lista com a coluna complexa separada pelos separadores
+  l_separada <- str_split(coluna_complexa,separadores)
+  #print(l_separada)
+  
+  # cria indice
+  indice <- 1:length(coluna_id)
+  
+  # cria tabela de retorno
+  for(i in indice){
+      #print(i)
+    
+      for(j in l_separada[[i]]){
+          
+          #print(j)
+          
+          if(exists("tabela")){
+            tabela <- bind_rows(tabela,data.frame(coluna_id[i],j))
+          } else{
+            tabela <- data.frame(coluna_id[i],j)
+          }
+          
+      }
+    
+    tabela$j <- str_trim(tabela$j)
+    
+  }
+  
+  #print(tabela)
+  names(tabela) <- c("identificador","info")
+  return(tabela)
+  
+  
+}
